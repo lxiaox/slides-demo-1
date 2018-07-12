5eb9bed048df81c9a7d06330803d4c1e49a06c2d
lxiaox <1512755884@qq.com>
2018-07-12 09:50:24 +0800

    代码优化

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var allButtons = $('#buttons>span')
-console.log(allButtons[0]);
 for(let i = 0; i<allButtons.length; i++){
     $(allButtons[i]).on('click',(x)=>{
         let index = $(x.currentTarget).index()
@@ -7,9 +6,8 @@ for(let i = 0; i<allButtons.length; i++){
         $('#images').css({
             transform: 'translateX('+ p +'px)'
         })
-        n = index //使定时切换按照新的顺序
-        allButtons.eq(index).addClass('selected')
-            .siblings('.selected').removeClass('selected')
+        n = index //使自动播放按照新的顺序
+        activeButton(allButtons.eq(n))
 
     })
 }
@@ -17,10 +15,8 @@ for(let i = 0; i<allButtons.length; i++){
 var size = allButtons.length
 var n = 0
 
-var timerId = setInterval(()=>{//先等3s再执行n+=1，n%size=1、2、0、1
-    n += 1
-    allButtons.eq(n%size).trigger('click')
-},3000)
+var timerId = setTimer()
+
 
 $('.window').on('mouseenter',() => {
     window.clearInterval(timerId)
@@ -28,11 +24,21 @@ $('.window').on('mouseenter',() => {
 $('.window').on('mouseleave',() => {
     n += 1
     allButtons.eq(n%size).trigger('click')//光标移走后图片立即切换效果，不管停留在该图片的时间大于小于3s
-    timerId = setInterval(()=>{
+    timerId = setTimer()
+})
+
+/****************************************/
+function setTimer(){
+    return setInterval(()=>{//先等3s再执行n+=1，n%size=1、2、0、1
         n += 1
         allButtons.eq(n%size).trigger('click')
     },3000)
-})
+}
+function activeButton($button){
+    $button.addClass('selected')
+        .siblings('.selected').removeClass('selected')
+}
+
 
 
 
